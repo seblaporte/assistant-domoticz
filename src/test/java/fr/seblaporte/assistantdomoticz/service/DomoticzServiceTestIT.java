03f31a475933d709e3696cf630b9d33b580c0d2b
sebastien.laporte <seb_laporte_33@yahoo.com>
2018-11-16 18:51:31 +0100

    Correction problème run tests sur GAE

--- src/test/java/fr/seblaporte/assistantdomoticz/service/DomoticzServiceTestIT.java
+++ src/test/java/fr/seblaporte/assistantdomoticz/service/DomoticzServiceTestIT.java
@@ -2,13 +2,18 @@ package fr.seblaporte.assistantdomoticz.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class DomoticzServiceTestIT {
+
+    @Autowired
+    private DomoticzService domoticzService;
 
     @Test
     public void should_return_devices_from_domoticz() throws Exception {
-
-        DomoticzService domoticzService = new DomoticzService();
 
         Assertions.assertThat(domoticzService.getDevices()).isNotEmpty();
     }
